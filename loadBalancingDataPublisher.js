8709d87e6c615fad96e4bdaa797ad03df6ac3030
gayanlggd <gayand@wso2.com>
2013-06-25 12:08:42 +0530

    Update loadBalancingDataPublisher.js

--- loadBalancingDataPublisher.js
+++ loadBalancingDataPublisher.js
@@ -1,4 +1,3 @@
-
 var DataPublisherHolder = Packages.org.wso2.carbon.databridge.agent.thrift.lb.DataPublisherHolder;
 var ReceiverGroup = Packages.org.wso2.carbon.databridge.agent.thrift.lb.ReceiverGroup;
 var LoadBalancingDataPublisher = Packages.org.wso2.carbon.databridge.agent.thrift.lb.LoadBalancingDataPublisher;
@@ -6,7 +5,7 @@ var ArrayList = Packages.java.util.Arraylist;
 
 
 //Details about receiver groups can be found here http://docs.wso2.org/wiki/display/BAM230/Setting+up+Multi+Receiver+and+Load+Balancing++Data+Agent
-function createLoadBalancingDataPublisher(receiverGroupUrls){
+function createLoadBalancingDataPublisher(receiverGroupUrls,username,password){
     var allReceiverGroups = new ArrayList();
 
     for (var i=0 ; i< receiverGroupUrls.length ; i++){
@@ -14,7 +13,7 @@ function createLoadBalancingDataPublisher(receiverGroupUrls){
         var urls = receiverGroupUrls[i].split(",");
 
         for(var j=0 ; j< urls.length ; i++) {
-        var aNode =    new DataPublisherHolder(null, urls[i].trim() , "admin", "admin");
+        var aNode =    new DataPublisherHolder(null, urls[i].trim() , username, password);
         dataPublisherHolders.add(aNode);
          }
         var receiverGroup = new ReceiverGroup(dataPublisherHolders);
